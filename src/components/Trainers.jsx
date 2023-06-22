42a29dd772d543e285ad91d2719b35560ca474ce
Phuc Mai <mariamaihong1309@gmail.com>
2023-06-22 16:47:32 -0400

    fourth

--- src/components/Trainers.jsx
+++ src/components/Trainers.jsx
@@ -1,64 +1,55 @@
 import React from "react";
+import HeroPages from "./HeroPages";
+
+import Trainer1 from "../images/trainer1.png";
+import Trainer2 from "../images/trainer2.png";
+import Trainer3 from "../images/trainer3.png";
 
 const Trainers = () => {
-  // const TrainerData = [
-  //   {
-  //     name: "John Lewis",
-  //     position: "Yoga Trainer",
-  //     img: Trainer1,
-  //   },
-  //   {
-  //     name: "Jonathan Doe",
-  //     position: "Crossfit Trainer",
-  //     img: Trainer2,
-  //   },
-  //   {
-  //     name: "Ana June",
-  //     position: "Personal Trainer",
-  //     img: Trainer3,
-  //   },
-  // ];
+  const TrainerData = [
+    {
+      name: "John Lewis",
+      position: "Yoga Trainer",
+      img: Trainer1,
+    },
+    {
+      name: "Jonathan Doe",
+      position: "Crossfit Trainer",
+      img: Trainer2,
+    },
+    {
+      name: "Ana June",
+      position: "Personal Trainer",
+      img: Trainer3,
+    },
+  ];
 
   return (
     <section className="trainers">
-      <h3>GYM TRAINERS</h3>
-      <h1>Team Of Expert Coaches</h1>
-      <p>
-        Expert team of coaches helps you succeed in any goal, personalized
-        guidance and motivation provided!
-      </p>
-      <div className="trainers-info">
-        <div className="trainer">
-          <div className="trainer-img">
-            <img src="images/trainers/trainer1.png" />
-          </div>
-          <div className="trainer-text">
-            <h2>John Lewis</h2>
-            <p>Yoga Trainer</p>
-          </div>
-        </div>
-
-        <div className="trainer">
-          <div className="trainer-img">
-            <img src="images/trainers/trainer2.png" />
-          </div>
-          <div className="trainer-text">
-            <h2>Jonathan Doe</h2>
-            <p>Crossfit Trainer</p>
-          </div>
-        </div>
-
-        <div className="trainer">
-          <div className="trainer-img">
-            <img src="images/trainers/trainer3.png" />
-          </div>
-          <div className="trainer-text">
-            <h2>Ana June</h2>
-            <p>Personal Trainer</p>
-          </div>
-        </div>
-        
+      <div className="trainers-overall">
+        <h3>GYM TRAINERS</h3>
+        <h1>Team Of Expert Coaches</h1>
+        <p>
+          Expert team of coaches helps you succeed in any goal, personalized
+          guidance and motivation provided!
+        </p>
       </div>
+
+      <div className="trainers-info">
+        {TrainerData.map((item) => (
+          <div className="trainer">
+            <div className="trainer-profile">
+              <img className="trainer-bg" src="images/trainers/trainer-bg.png" />
+              <img className="trainer-img" src={item.img} />
+            </div>
+            <div className="trainer-text">
+              <h1>{item.name}</h1>
+              <p>{item.position}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+
     </section>
   );
 };
